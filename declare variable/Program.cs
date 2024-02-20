f46e8cdbe12e3ff63b3b720cd6238780642b723e
fm Bushry <fmbushry14@gmail.com>
2024-02-20 10:37:59 +0530

    Add project files.

---
+++ declare variable/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string firstname = "faleelullah mohamed";
+string lastname = "bushry";
+Console.WriteLine($"this is {firstname}{lastname}, from kinniya");
+
